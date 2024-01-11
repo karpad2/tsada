0ef958098423d9671fcfcd2cb8ffec507c4bffb9
Árpád Kovács <karpad2@gmail.com>
2024-01-11 01:34:57 +0100

    aaa

--- src/appwrite/index.js
+++ src/appwrite/index.js
@@ -1,9 +1,18 @@
 import { Client,Account } from 'appwrite';
 import config from './config.json';
 const appw = new Client();
+if(false)
+{
 appw
     .setEndpoint('https://appwrite.tsada.edu.rs/v1')
     .setProject('659ea7f886cf55d4528a');
+}
+else
+{
+    appw
+    .setEndpoint('https://appwrite.kasoft.co.uk/v1')
+    .setProject('653bb473ee7f6a6074e7');
+}
 const user=new Account(appw);
 let loggedIn=false;
 //console.log(user.get());
