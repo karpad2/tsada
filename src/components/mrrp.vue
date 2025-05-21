13bc02ffc4bb58a2507dcbbb85b905ed9c4c93f6
Árpád Kovács <karpad2@gmail.com>
2025-05-21 19:39:55 +0200

    aaa

--- src/components/mrrp.vue
+++
@@ -1,7 +0,0 @@
-<template>
-<div class="mx-auto">
-    <img src="@a/profileBanner_dzuderbbkat91.webp" />óó
-</div>
-</template>
-<script>
-</script>
--- src/plugins/vuetify.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import { createVuetify } from 'vuetify'
-
-export default createVuetify({
-  theme: {
-    defaultTheme: 'dark',
-  },
-})
